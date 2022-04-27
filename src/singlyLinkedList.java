54a4539641ea36152ca61810532a379c4e81930e
Isha Gupta <isha.gupta_cs19@gla.ac.in>
2022-04-27 23:42:54 +0530

    linkedlist

--- src/singlyLinkedList.java
+++ src/singlyLinkedList.java
@@ -27,7 +27,81 @@ public class singlyLinkedList {
         }
         return count;
     }
+    // insert node at the beginning of Singlylinkedlist
+    public void insert(int data){
+        listNode newNode=new listNode(data);
+        newNode.next=head;
+        head=newNode;
 
+    }
+    // insert node at the end of singlylinkedlist
+    public void insertAtEnd(int value){
+        listNode newNode=new listNode(value);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        listNode current=head;
+        while(current.next!=null){
+            current=current.next;
+
+        }
+        current.next=newNode;
+    }
+public void insertAtPosition(int value,int position){
+        listNode newNode=new listNode(value);
+        if(position==1){
+            newNode.next=head;
+            head=newNode;
+        }
+        else {
+            listNode previous = head;
+            int count = 1;
+            while (count < position - 1) {
+                previous = previous.next;
+                count++;
+            }
+            listNode current = previous.next;
+            newNode.next = current;
+            previous.next = newNode;
+        }
+}
+public listNode deleteAtBeginning(){
+        if(head==null){
+            return null;
+        }
+        listNode temp=head;
+        head=head.next;
+        temp.next=null;
+        return temp;
+}
+public listNode deleteAtEnd(){
+        if(head==null || head.next==null){
+            return head;
+        }
+        listNode current=head;
+        listNode previous=null;
+        while(current.next!=null){
+            previous=current;
+            current=current.next;
+        }
+        previous.next=null;
+        return current;
+}
+public listNode deleteAtPosition(int position){
+        if(position==1){
+            head=head=head.next;
+        }
+        listNode previous=head;
+        int count=1;
+        while(count<position-1){
+            previous=previous.next;
+            count++;
+        }
+        listNode current=previous.next;
+        previous.next=current.next;
+        return current;
+}
 
     public static void main(String[] args) {
         singlyLinkedList obj = new singlyLinkedList();
@@ -40,5 +114,17 @@ public class singlyLinkedList {
         third.next=fourth;
         obj.display();
         System.out.println(obj.length());
+        obj.insert(11);
+        obj.display();
+        obj.insertAtEnd(22);
+        obj.display();
+        obj.insertAtPosition(33,5);
+obj.display();
+obj.deleteAtBeginning();
+obj.display();
+obj.deleteAtEnd();
+obj.display();
+obj.deleteAtPosition(4);
+obj.display();
     }
     }
